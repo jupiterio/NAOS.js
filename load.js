be13d4d3d20843ce18d8fba38321c8e0ffa3a301
jupiterio <ioOnScratch@gmail.com>
2017-07-16 20:09:28 -0600

    Add placeholder page

--- load.js
+++ load.js
@@ -1,10 +1,15 @@
 window.addEventListener("load", function(){
+  document.body.innerHTML = "";
+  
   var taskbar = document.createElement("div");
   taskbar.id = "taskbar";
-  taskbar.innerHTML = `<input type="button" onclick="new Window(Math.floor(Math.random()*(100-40))+40, Math.floor(Math.random()*(100-40))+40, Math.floor(Math.random()*(400-180))+180, Math.floor(Math.random()*(400-180))+180, {content: document.createTextNode('Hello, World!')})">`;
+  /* temporary */ taskbar.innerHTML = `<input type="button" onclick="new Window(Math.floor(Math.random()*(100-40))+40, Math.floor(Math.random()*(100-40))+40, Math.floor(Math.random()*(400-180))+180, Math.floor(Math.random()*(400-180))+180, {content: document.createTextNode('Hello, World!')})">`;
   document.body.appendChild(taskbar);
   
   var mainScript = document.createElement("script");
   mainScript.src = "OS.js";
   document.body.appendChild(mainScript);
+  
+  document.getElementById("page-style").href = "OS.css";
+  document.getElementById("page-fonts").href = "https://fonts.googleapis.com/css?family=Open+Sans|Share";
 });
